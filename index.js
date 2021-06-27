6f83cf47b6d0f0870a46ee7f2582d46bfad994b3
Yurri <brechkoio@gmail.com>
2021-06-27 14:42:41 +0300

    add views folder

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello NodeJS');
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
