b0fc953c0f6e0ff30d71d103892b4428f19854e4
Yurri <brechkoio@gmail.com>
2021-06-27 14:57:25 +0300

    instaled express-handlebars

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const express = require('express');
+var exphbs  = require('express-handlebars');
 const path = require('path');
 const app = express();
 
+const hbs = exphbs.create({
+    defaultLayout: 'main',
+    extname: 'hbs'
+});
+
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', 'views');
+
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+    res.render('index');
+});
+
+app.get('/about', (req, res) => {
+    res.render('about');
 });
 
 const PORT = process.env.PORT || 3000;
