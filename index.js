0d1c4076a53d047acf8854afb57d307929a853ed
Yurri <brechkoio@gmail.com>
2021-06-27 15:30:36 +0300

    add active links for navbar

--- index.js
+++ index.js
@@ -12,12 +12,27 @@ app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
 app.set('views', 'views');
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('index', {
+        title: 'Home',
+        isHome: true
+    });
 });
 
-app.get('/about', (req, res) => {
-    res.render('about');
+app.get('/courses', (req, res) => {
+    res.render('courses', {
+        title: 'Courses',
+        isCourses: true
+    });
+});
+
+app.get('/add', (req, res) => {
+    res.render('add', {
+        title: 'Add Course',
+        isAdd: true
+    });
 });
 
 const PORT = process.env.PORT || 3000;
