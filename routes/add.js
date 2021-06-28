a8e52a5b63ce1aea9e8e6301e20251b1f8c8ed43
Yurri <brechkoio@gmail.com>
2021-06-28 12:08:33 +0300

    add couse model

--- routes/add.js
+++ routes/add.js
@@ -1,4 +1,5 @@
 const { Router } = require('express');
+const Course = require('../models/course');
 const router = Router();
 
 router.get('/', (req, res) => {
@@ -8,8 +9,11 @@ router.get('/', (req, res) => {
     });
 });
 
-router.post('/', (req, res) => {
-    console.log(req.body);
+router.post('/', async (req, res) => {
+    const { title, price, img} = req.body;
+    const course = new Course(title, price, img);
+
+    await course.save();
 
     res.redirect('/courses');
 });
